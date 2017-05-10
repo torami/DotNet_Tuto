721e1c2a23e46965069c71627d16a0355759b57d
rami.torkhani <rami.torkhani@BLPP-4818>
2017-05-10 12:38:19 +0200

    add an authentication

--- MyFirstWebsite/Controllers/AuthController.cs
+++ MyFirstWebsite/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyFirstWebsite.Models;
+using System.Security.Claims;
 
 namespace MyFirstWebsite.Controllers
 {
@@ -11,6 +12,7 @@ namespace MyFirstWebsite.Controllers
     public class AuthController : Controller
     {
         // GET: Auth
+        [HttpGet]
         public ActionResult Login(string retrunUrl)
         {
             var model = new LoginModel
@@ -18,7 +20,34 @@ namespace MyFirstWebsite.Controllers
                 ReturnUrl = retrunUrl
             };
 
-            return View();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+                     return View(model);
+        }
+        public ActionResult LogIn (LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            if (model.Email == "admin@admin.com" && model.Password == "12345")
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name,"Rami"),
+                    new Claim(ClaimTypes.Email,"Rami@email.com"),
+                    new Claim(ClaimTypes.Country,"France") }, "ApplicationCookie"
+                );
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+                authManager.SignIn(identity);
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+            ModelState.AddModelError("", "Invalid email or password");
+            return View(model);
         }
     }
 }
